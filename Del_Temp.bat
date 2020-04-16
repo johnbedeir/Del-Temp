92b1fe710c656505fccea570a6cb864af767e381
triplea-git <triplea@jb-host.net>
2020-04-16 06:36:55 +0200

    readme file

--- Del_Temp.bat
+++ Del_Temp.bat
@@ -3,9 +3,6 @@
 #description 	: This Script is to delete Temp Files
 #author 	:JohnBedeir
 #date		:16Apr20
-#notes		:Copy this file to drive (C), and make a shortcut to the Startup Folder
-#Edit		:Right-Click and edit the file, replace "$USERNAME" with your account username
-#Access Startup Folder by typing "shell:common startup" in RUN
 
 @echo off
 DEL /S /Q C:\Users\$USERNAME\AppData\Local\Temp\*.*
---
+++ README.txt
@@ -0,0 +1,8 @@
+#notes:
+ 
+1. Right-Click and edit the file, replace "$USERNAME" with your account username
+2. Copy this file to drive (C), and make a shortcut to the Startup Folder
+3. Access Startup Folder by typing "shell:common startup" in RUN
+
+Have fun.
+
